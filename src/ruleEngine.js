7de9c0890b5024d09481617607b5bbeacad15186
Nick de Jong <nick.dejong@morningstar.com>
2019-09-13 11:25:58 -0500

    refactored to have ruleEngine import ruleTree

--- src/ruleEngine.js
+++ src/ruleEngine.js
@@ -1,50 +1,9 @@
-const AND = 'AND'
-const OR = 'OR'
-const XOR = 'XOR'
+const { RuleTree } = require('./ruleTree')
 
-const ruleHandling = (rule) => {
-  switch (typeof rule) {
-    case 'function':
-      return rule();
-    case 'boolean':
-      return rule
-    case 'object':
-      return engine(rule)
-  }
+const ruleEngine = (rulesObject) => {
+  const ruleTree = new RuleTree()
+  ruleTree.build(rulesObject)
+  return ruleTree
 }
 
-const andRule = rulesArray => {
-  return rulesArray.every(ruleHandling)
-}
-
-const orRule = rulesArray => {
-  return rulesArray.some(ruleHandling)
-}
-
-const xorRule = rulesArray => {
-  return rulesArray.map(ruleHandling)
-    .filter(result => result).length === 1
-}
-
-/* 
-structure of a ruleSet
-{
-  type: String, // enum ['AND', 'OR', 'XOR']
-  rules: [Boolean, Function returning boolean, ruleSet]
-}
-
-
-*/
-const engine = (ruleSet) => {
-  const { type = 'AND' } = ruleSet
-  const methodMap = {
-    AND: andRule,
-    OR: orRule,
-    XOR: xorRule
-  }
-  const method = methodMap[type] || methodMap.AND
-  // const array = new RulesArray(ruleSet.rules)
-  return method(ruleSet.rules)
-}
-
-module.exports = engine
+module.exports = ruleEngine
