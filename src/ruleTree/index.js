2935c87ee65864c3dfb308b9ad2ae0538ebf59ad
Nick de Jong <nick.dejong@morningstar.com>
2019-10-15 09:37:39 -0500

    exporting rule node and analysis node

--- src/ruleTree/index.js
+++
@@ -1,4 +0,0 @@
-
-const RuleTree = require('./RuleTree')
-  
-module.exports = RuleTree
---
+++ src/ruleTree/index.ts
@@ -0,0 +1,8 @@
+
+import AnalysisNodeClass from './analysisNode'
+import RuleNodeClass from './ruleNode'  
+import RuleTree from './ruleTree'
+
+export const AnalysisNode = AnalysisNodeClass
+export const RuleNode = RuleNodeClass
+export default RuleTree
